a307ec3f1a8d5edef77a3a9315adee5831e66f95
Warren E. Downs <vwdowns@gmail.com>
2019-01-29 16:59:16 -0900

    Fixes for ChromeOS+crouton+Android

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-which npm || sudo apt install npm
-which pnpm || sudo npm install -g pnpm
+which node >/dev/null || sudo apt install node
+which npm  >/dev/null || sudo apt install npm
+which pnpm >/dev/null || sudo npm install -g pnpm
 pnpm install colors console-ten watchr websocket better-sqlite3 node-ipc command-line-args errlop editions
