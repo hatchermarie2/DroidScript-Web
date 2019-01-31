5051ac02121497b96dcd99a8cac248f32cc5e477
Warren E. Downs <vwdowns@gmail.com>
2019-01-31 11:09:15 -0900

    ChromeOS+crouton+Android integration

--- scripts/checkin.sh
+++ scripts/checkin.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-find -name '.appcache' -exec rm {} \;
 [ "$1" = "" ] && echo "What is this checkin about?  Supply a description." && exit 1
+
+# Clean up temporary files
+find -name '.appcache' -exec rm {} \;
+# Clean up links in the sdcard folder (recreated on running ./dstart)
+find sdcard -type l -exec rm {} \;
 
 git commit -a -m "$1"
