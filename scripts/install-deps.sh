23a9f178536506aa4e72ed032577891179051e55
hatchermarie2 <50732645+hatchermarie2@users.noreply.github.com>
2019-08-11 19:54:36 -0500

    Update install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/should
 
 which node >/dev/null || sudo apt install node
 which npm  >/dev/null || sudo apt install npm
 which pnpm >/dev/null || sudo npm install -g pnpm
 pnpm install colors console-ten watchr websocket better-sqlite3 node-ipc command-line-args errlop editions
+</p>
+Script
